8b1621c64b59fa8fd19c0eb38af7b89616ebb737
Kitto Adinatha <Kitto@Kittos-MacBook-Pro.local>
2016-09-20 14:44:36 +0700

    finished django tutorial 1

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,9 +1,6 @@
-from django.conf.urls import include, url
-from django.contrib import admin
+from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^polls/', include('polls.urls')),
     url(r'^$', views.index, name='index')
 ]
