27bf197eb20f35ca6708b5bfe57395247bd0928f
Bimo Prabowo Reksoprodjo <Bimo Prabowo Reksoprodjo>
2016-09-26 20:37:08 +0700

    Finished 2nd Django tutorial

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,3 +1,27 @@
-from django.db import models
-
 # Create your models here.
+from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
+import datetime
+
+
+@python_2_unicode_compatible
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.question_text
+
+    # Custom function
+    def was_published_recently(self):
+        return self.pub_date >= datetime.now() - datetime.timedelta(days=1)
+
+
+@python_2_unicode_compatible
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
