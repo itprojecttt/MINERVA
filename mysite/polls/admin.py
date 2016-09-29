0c689b974dca2c9b1b8a21ce52953aae867d65ea
Bimo Prabowo Reksoprodjo <Bimo Prabowo Reksoprodjo>
2016-09-29 08:59:47 +0700

    Finished introductory tutorial of Django.

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,6 +1,23 @@
 from django.contrib import admin
-from .models import Question
+from .models import Choice, Question
 
 
 # Register your models here.
-admin.site.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
+
+class ChoiceInline(admin.TabularInline):
+    fieldsets = [
+        (None, {'fields': ['choice_text']}),
+        ('Date information', {'fields': ['votes']}),
+    ]
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,6 +1,7 @@
 # Create your models here.
 from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
+from django.utils import timezone
 import datetime
 
 
@@ -14,8 +15,12 @@ class Question(models.Model):
 
     # Custom function
     def was_published_recently(self):
-        now = datetime.timezone.now()
+        now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
 
 
 @python_2_unicode_compatible
