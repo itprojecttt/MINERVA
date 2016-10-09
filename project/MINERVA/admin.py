ff3ceea0c77fdf51d2d0ec85a330257d0a4f502a
Kitto Adinatha <Kitto@Kittos-MacBook-Pro.local>
2016-10-09 20:38:12 +0700

    cleaned up conflicts

--- project/MINERVA/admin.py
+++ project/MINERVA/admin.py
@@ -8,16 +8,5 @@ class MilestoneAdmin(admin.ModelAdmin):
     list_display = ('milestone', 'start', 'seven_five', 'finish')
 
 
-<<<<<<< HEAD
 admin.site.register(Milestone, MilestoneAdmin)
-=======
-class ChoiceInline(admin.TabularInline):
-    fieldsets = [
-        ('Milestone', {'fields': ['uid_milestone']}),
-        ('User', {'fields': ['uid_user']}),
-        ('Timestamp', {'fields': ['timestamp']})
-    ]
-
-admin.site.register(Milestone)
->>>>>>> 41e57f43589edd20bcc7bc14e2ee8efa6be550fe
 admin.site.register(UserChecklist)
