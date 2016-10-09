000b61d93b7d2033b59be74256a25b75039bbfd6
r <r@rs-MacBook-Pro.local>
2016-10-09 14:33:16 +0700

    form table implementation

--- frontend/js/dynamic-table-t.js
+++ frontend/js/dynamic-table-t.js
@@ -8,9 +8,9 @@ function addRow() {
     var rowCount = table.rows.length;
     var row = table.insertRow(rowCount);
 
-    row.insertCell(0).innerHTML= height.value;
-    row.insertCell(1).innerHTML= weight.value;
-	    row.insertCell(2).innerHTML= date.value;
+    row.insertCell(0).innerHTML= String(weight.value) + ' <br> <input type="hidden" name="inputWeight' + String(rowCount) + '" value="' + String(weight.value) + '" />';
+    row.insertCell(1).innerHTML= String(height.value) + ' <br> <input type="hidden" name="inputHeight' + String(rowCount) + '" value="' + String(height.value) + '" />';
+	row.insertCell(2).innerHTML= String(date.value) + ' <br> <input type="hidden" name="inputDate' + String(rowCount) + '" value="' + String(date.value) + '" />';;
 	row.insertCell(3).innerHTML= '<input type="button" value = "Delete" onClick="Javacsript:deleteRow(this)">';
 
 }
